71da2932350839c6b26fbd8de071d39eb36e13c2
Manuel <manuelhany@gmail.com>
2023-10-18 12:56:33 +0300

    include snippets

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,7 +4,7 @@ from . import views
 # if a request reaches /january ==> then execute, the views.index function. 
 # this is called url config or url
 urlpatterns = [
-    path("", views.index), # /challenges/
+    path("", views.index, name="index"), # /challenges/
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
